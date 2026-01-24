c55d608e8e435ff9050cfcd42d64375827fecf34
wenyi <jwy600@gmail.com>
2026-01-25 01:55:29 +0800

    chore: remove unused index.ts barrel files

--- components/landing/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Landing Page Components - Hero, thread list, and landing page elements
- */
-
-export { Hero } from './Hero';
-export { ThreadList } from './ThreadList';
-export { ThreadPill } from './ThreadPill';
--- components/sidebar/index.ts
+++
@@ -1,4 +0,0 @@
-export { AppSidebar } from './AppSidebar';
-export { SidebarLogo } from './SidebarLogo';
-export { NewChatButton } from './NewChatButton';
-export { SidebarThreadList } from './SidebarThreadList';
